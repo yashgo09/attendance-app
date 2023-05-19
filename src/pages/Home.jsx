e7c02e6f1d2c4422595826026555f8b656301c64
Yash Gorasia <109784551+yashgo09@users.noreply.github.com>
2023-05-19 13:07:14 +0530

    Created context for Attendance

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import data from "../data/students.json";
+import { AttendanceContext } from "../contexts";
 
 function Home() {
   let key = 0;
 
-  const [attendance, setAttendance] = useState({});
+  const { handleClick } = useContext(AttendanceContext);
 
-  const handleClick = (e) => {
-    const date = document.querySelector("#date");
-    console.log(date.value);
-    if (date.value === "") {
-      console.error("Please select Date");
-      alert("Please select Date");
-      date.focus();
-      return;
-    }
-    const presentCheckboxes = [...document.querySelectorAll(".present-checkbox")];
-    const presentStudents = presentCheckboxes
-      .filter((checkbox) => checkbox.checked)
-      .map((box) => box.value);
-
-    const absentStudents = presentCheckboxes
-      .filter((checkbox) => !checkbox.checked)
-      .map((box) => box.value);
-    setAttendance({ date: date.value, presentStudents, absentStudents });
-
-    console.log("Success");
-  };
-
-  console.log(attendance);
   return (
     <div className="container">
       <input type="date" name="date" id="date" />
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
+import { AttendanceContext } from "../contexts";
 
 function Root() {
+  const [attendance, setAttendance] = useState({});
+
+  const handleClick = (e) => {
+    const date = document.querySelector("#date");
+    console.log(date.value);
+    if (date.value === "") {
+      console.error("Please select Date");
+      alert("Please select Date");
+      date.focus();
+      return;
+    }
+    const presentCheckboxes = [...document.querySelectorAll(".present-checkbox")];
+    const presentStudents = presentCheckboxes
+      .filter((checkbox) => checkbox.checked)
+      .map((box) => box.value);
+
+    const absentStudents = presentCheckboxes
+      .filter((checkbox) => !checkbox.checked)
+      .map((box) => box.value);
+    setAttendance({ date: date.value, presentStudents, absentStudents });
+
+    console.log("Success");
+  };
+
+  console.log(attendance);
+
   return (
-    <>
+    <AttendanceContext.Provider value={{ handleClick }}>
       <Header />
       <Outlet />
-    </>
+    </AttendanceContext.Provider>
   );
 }
 
