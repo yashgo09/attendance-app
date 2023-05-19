271e043e1cf44abd0b03da2d3ac526f3b7c7476b
Yash Gorasia <109784551+yashgo09@users.noreply.github.com>
2023-05-19 12:29:40 +0530

    Local state working for attendance

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "../data/students.json";
 
 function Home() {
+  let key = 0;
+
+  const [attendance, setAttendance] = useState({});
+
+  const handleClick = (e) => {
+    const date = document.querySelector("#date");
+    console.log(date.value);
+    if (date.value === "") {
+      console.error("Please select Date");
+      alert("Please select Date");
+      date.focus();
+      return;
+    }
+    const presentCheckboxes = [...document.querySelectorAll(".present-checkbox")];
+    const presentStudents = presentCheckboxes
+      .filter((checkbox) => checkbox.checked)
+      .map((box) => box.value);
+
+    const absentStudents = presentCheckboxes
+      .filter((checkbox) => !checkbox.checked)
+      .map((box) => box.value);
+    setAttendance({ date: date.value, presentStudents, absentStudents });
+
+    console.log("Success");
+  };
+
+  console.log(attendance);
   return (
     <div className="container">
+      <input type="date" name="date" id="date" />
       <div className="students">
-        {data.map((data) => (
-          <>
-            <p>{data.id}</p>
-            <p>{data.name}</p>
-          </>
-        ))}
+        <ul className="students__list">
+          {data.map((data) => (
+            <li key={key++}>
+              <p>{data.name}</p>
+              <label>
+                <input
+                  className="present-checkbox"
+                  type="checkbox"
+                  value={data.id}
+                  name="present"
+                />
+                Present
+              </label>
+            </li>
+          ))}
+        </ul>
       </div>
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
